eb6b68a4e6af24043fe66481523a3bebfc937d83
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-03-13 00:03:20 +0600

    added blogs json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl bg-red-400'>React Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
